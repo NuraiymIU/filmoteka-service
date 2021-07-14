3689243060b5b4ffcde44988fe71483c2566ff68
NURAIYM <nurbekovanuraiym2000@gmail.com>
2021-07-14 19:14:17 +0600

    Initial commit

---
+++ src/main/java/kg/megacom/filmotekaservice/FilmotekaServiceApplication.java
@@ -0,0 +1,13 @@
+package kg.megacom.filmotekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmotekaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmotekaServiceApplication.class, args);
+    }
+
+}
